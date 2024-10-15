d8fd8994235b4f622825bb5df72055ec4ffdad43
Riley Bruins <ribru17@hotmail.com>
2024-10-15 09:54:32 -0700

    feat: minimal LSP implementation

---
+++ src/main.rs
@@ -0,0 +1,91 @@
+use std::{env::set_current_dir, path::PathBuf};
+
+use dashmap::DashMap;
+use ropey::Rope;
+use tower_lsp::{jsonrpc::Result, lsp_types::*, Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+    _document_map: DashMap<Url, Rope>,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, params: InitializeParams) -> Result<InitializeResult> {
+        self.client
+            .log_message(
+                MessageType::LOG,
+                format!("ts_query_ls initialize: {:?}", params),
+            )
+            .await;
+        if let Some(root_uri) = params.root_uri {
+            let root = PathBuf::from(root_uri.path());
+            if set_current_dir(&root).is_err() {
+                self.client
+                    .log_message(
+                        MessageType::ERROR,
+                        format!("Failed to set root directory to {:?}", root),
+                    )
+                    .await;
+            };
+        }
+
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities {
+                // text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                //     TextDocumentSyncKind::INCREMENTAL,
+                // )),
+                // hover_provider: Some(HoverProviderCapability::Simple(true)),
+                definition_provider: Some(OneOf::Left(true)),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        self.client
+            .log_message(MessageType::LOG, "ts_query_ls shutdown".to_string())
+            .await;
+        Ok(())
+    }
+
+    async fn goto_definition(
+        &self,
+        params: GotoDefinitionParams,
+    ) -> Result<Option<GotoDefinitionResponse>> {
+        self.client
+            .log_message(
+                MessageType::LOG,
+                format!("ts_query_ls goto_definition: {:?}", params),
+            )
+            .await;
+        let uri = params.text_document_position_params.text_document.uri;
+        let pos = Position {
+            line: params.text_document_position_params.position.line,
+            character: 0,
+        };
+        let range = Range::new(pos, pos);
+        Ok(Some(GotoDefinitionResponse::Scalar(Location {
+            uri,
+            range,
+        })))
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::build(|client| Backend {
+        client,
+        _document_map: DashMap::new(),
+    })
+    .finish();
+
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
