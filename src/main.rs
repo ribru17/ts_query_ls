64c89de5869c48f16d6022abbcccc793bd1e816a
WillLillis <wlillis@umass.edu>
2024-11-17 21:30:11 -0500

    feat: implement goto definition for captures

--- src/main.rs
+++ src/main.rs
@@ -364,14 +364,65 @@ impl LanguageServer for Backend {
         params: GotoDefinitionParams,
     ) -> Result<Option<GotoDefinitionResponse>> {
         info!("ts_query_ls goto_definition: {params:?}");
-        let uri = params.text_document_position_params.text_document.uri;
-        let pos = Position {
-            line: params.text_document_position_params.position.line,
-            character: 0,
+        let uri = &params.text_document_position_params.text_document.uri;
+        let Some(tree) = self.ast_map.get(uri) else {
+            warn!("No AST built for URI: {uri:?}");
+            return Ok(None);
         };
-        let range = Range::new(pos, pos);
-        Ok(Some(GotoDefinitionResponse::Scalar(Location {
+        let Some(rope) = self.document_map.get(uri) else {
+            warn!("No document built for URI: {uri:?}");
+            return Ok(None);
+        };
+        let cur_pos = params.text_document_position_params.position;
+        let Some(current_node) =
+            get_current_capture_node(tree.root_node(), lsp_position_to_ts_point(cur_pos, &rope))
+        else {
+            return Ok(None);
+        };
+
+        let query = Query::new(&QUERY_LANGUAGE, "(capture) @cap").unwrap();
+        let mut cursor = QueryCursor::new();
+        let provider = TextProviderRope(&rope);
+
+        let mut parser = Parser::new();
+        parser
+            .set_language(&QUERY_LANGUAGE)
+            .expect("Error setting language for Query parser");
+
+        let Some(def) = get_references(
             uri,
+            &tree.root_node(),
+            &current_node,
+            &query,
+            &mut cursor,
+            &provider,
+            &rope,
+        )
+        .min_by(|c1, c2| {
+            if c1 == c2 {
+                return std::cmp::Ordering::Equal;
+            }
+            let first_start_line = c1.range.start.line;
+            let elem_start_line = c2.range.start.line;
+            if (elem_start_line < first_start_line)
+                || (elem_start_line == first_start_line
+                    && c2.range.start.character < c1.range.start.character)
+            {
+                std::cmp::Ordering::Greater
+            } else {
+                std::cmp::Ordering::Less
+            }
+        }) else {
+            let range = Range::new(cur_pos, cur_pos);
+            return Ok(Some(GotoDefinitionResponse::Scalar(Location {
+                uri: uri.clone(),
+                range,
+            })));
+        };
+
+        let range = Range::new(def.range.start, def.range.end);
+        Ok(Some(GotoDefinitionResponse::Scalar(Location {
+            uri: uri.clone(),
             range,
         })))
     }
