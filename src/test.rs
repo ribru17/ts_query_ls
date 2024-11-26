913e4ab58543e876b22e4a18bd422fb2b8a63202
WillLillis <wlillis@umass.edu>
2024-11-25 22:54:40 -0500

    fix: refactor tests
    Co-authored-by: Riley Bruins <ribru17@hotmail.com>

--- src/test.rs
+++ src/test.rs
@@ -1,20 +1,86 @@
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
+    use lazy_static::lazy_static;
+    use std::sync::Arc;
 
-    use ropey::Rope;
-    use tower_lsp::lsp_types::{Position, Range, Url};
-    use tree_sitter::{Query, QueryCursor};
-
-    use crate::{
-        util::{
-            get_current_capture_node, get_references, lsp_position_to_ts_point,
-            ts_node_to_lsp_location, TextProviderRope,
+    use dashmap::DashMap;
+    use std::sync::RwLock;
+    use tower_lsp::{
+        lsp_types::{
+            ClientCapabilities, DidOpenTextDocumentParams, InitializeParams, PartialResultParams,
+            Position, Range, ReferenceContext, ReferenceParams, TextDocumentIdentifier,
+            TextDocumentItem, TextDocumentPositionParams, Url, WorkDoneProgressParams,
         },
-        QUERY_LANGUAGE,
+        LanguageServer, LspService,
     };
 
-    fn test_capture_references(input: &str) {
+    use tokio::sync::Mutex;
+
+    use crate::{Backend, Options};
+
+    lazy_static! {
+        static ref TEST_URI: Url = Url::parse("file:///tmp/test.scm").unwrap();
+    }
+
+    /// Initializes a mock instance of `ts_query_ls`
+    /// The result is wrapped in a `tokio::sync::Mutex` for safe sharing
+    /// between threads
+    async fn initialize_test_server() -> Mutex<LspService<Backend>> {
+        let options = Arc::new(RwLock::new(Options {
+            parser_install_directories: None,
+            parser_aliases: None,
+            language_retrieval_patterns: None,
+        }));
+        let (service, _socket) = LspService::build(|client| Backend {
+            client,
+            document_map: DashMap::new(),
+            cst_map: DashMap::new(),
+            symbols_set_map: DashMap::new(),
+            symbols_vec_map: DashMap::new(),
+            fields_set_map: DashMap::new(),
+            fields_vec_map: DashMap::new(),
+            options,
+        })
+        .finish();
+
+        _ = service
+            .inner()
+            .initialize(InitializeParams {
+                capabilities: ClientCapabilities::default(),
+                root_uri: Some(Url::parse("file:///tmp/").unwrap()),
+                ..Default::default()
+            })
+            .await
+            .expect("Failed to initialize server");
+        service.into()
+    }
+
+    /// Mocks the test server opening a text document with `contents`
+    async fn open_document(server: &mut Mutex<LspService<Backend>>, contents: &str) {
+        server
+            .get_mut()
+            .inner()
+            .did_open(DidOpenTextDocumentParams {
+                text_document: TextDocumentItem {
+                    uri: TEST_URI.clone(),
+                    language_id: String::from("query"),
+                    version: 0,
+                    text: contents.to_string(),
+                },
+            })
+            .await;
+        assert!(
+            server
+                .get_mut()
+                .inner()
+                .document_map
+                .get(&TEST_URI)
+                .is_some(),
+            "Failed to insert contents of mock document"
+        );
+    }
+
+    async fn test_capture_references(input: &str) {
         let handle_cursor_marker =
             |input: &mut String,
              line_num: usize,
@@ -115,54 +181,51 @@ mod tests {
         let cursor_position =
             cursor_position.expect("textDocument/references test must contain one <CURSOR> marker");
 
-        let uri = Url::from_str("file://test.scm").unwrap();
-        let mut parser = tree_sitter::Parser::new();
-        parser
-            .set_language(&QUERY_LANGUAGE)
-            .expect("Failed to set language");
-        let tree = parser
-            .parse(&cleaned_input, None)
-            .expect("Failed to parse input");
-        let rope = Rope::from_str(&cleaned_input);
-        let provider = TextProviderRope(&rope);
-        let query = Query::new(&QUERY_LANGUAGE, "(capture) @cap").unwrap();
-        let mut cursor = QueryCursor::new();
-        let cur_pos = lsp_position_to_ts_point(cursor_position, &rope);
-        let Some(current_node) = get_current_capture_node(tree.root_node(), cur_pos) else {
-            panic!("No capture node found under cursor");
-        };
-
-        let found_refs = get_references(
-            &tree.root_node(),
-            &current_node,
-            &query,
-            &mut cursor,
-            &provider,
-            &rope,
-        )
-        .map(|node| ts_node_to_lsp_location(&uri, &node, &rope).range)
-        .collect::<Vec<Range>>();
+        let mut server = initialize_test_server().await;
+        open_document(&mut server, &cleaned_input).await;
+        let found_refs = server
+            .get_mut()
+            .inner()
+            .references(ReferenceParams {
+                text_document_position: TextDocumentPositionParams {
+                    text_document: TextDocumentIdentifier {
+                        uri: TEST_URI.clone(),
+                    },
+                    position: cursor_position,
+                },
+                work_done_progress_params: WorkDoneProgressParams::default(),
+                partial_result_params: PartialResultParams::default(),
+                context: ReferenceContext {
+                    include_declaration: true,
+                },
+            })
+            .await
+            .map_err(|e| format!("textDocument/references call returned error: {e}"))
+            .unwrap()
+            .expect("textDocument/references call returned None");
 
         let mut panic_msg = String::new();
         for reference in found_refs {
             let Some(idx) = expected_refs
                 .iter()
                 .enumerate()
-                .find(|(_, &r)| r == reference)
+                .find(|(_, &r)| r == reference.range)
                 .map(|(i, _)| i)
             else {
                 let line = cleaned_input
                     .lines()
-                    .nth(reference.start.line as usize)
+                    .nth(reference.range.start.line as usize)
                     .unwrap();
                 panic_msg += &format!(
                     "Found unexpected reference at position ({}, {}), ({}, {}):\n{line}\n{}{}\n",
-                    reference.start.line,
-                    reference.start.character,
-                    reference.end.line,
-                    reference.end.character,
-                    " ".repeat(reference.start.character as usize),
-                    "^".repeat((reference.end.character - reference.start.character) as usize),
+                    reference.range.start.line,
+                    reference.range.start.character,
+                    reference.range.end.line,
+                    reference.range.end.character,
+                    " ".repeat(reference.range.start.character as usize),
+                    "^".repeat(
+                        (reference.range.end.character - reference.range.start.character) as usize
+                    ),
                 );
                 continue;
             };
@@ -191,51 +254,51 @@ mod tests {
     /*
      *  tree-sitter-c queries
      */
-    #[test]
-    fn it_gives_references_for_captures_0() {
+    #[tokio::test]
+    async fn it_gives_references_for_captures_0() {
         let src = "(identifier) <REF>@variab<CURSOR>le";
-        test_capture_references(src);
+        test_capture_references(src).await;
     }
-    #[test]
-    fn it_gives_references_for_captures_1() {
+    #[tokio::test]
+    async fn it_gives_references_for_captures_1() {
         let src = r#"((identifier) <REF>@constant
-    (#match? <REF>@c<CURSOR>onstant "^[A-Z][A-Z\\d_]*$"))"#;
-        test_capture_references(src);
+        (#match? <REF>@c<CURSOR>onstant "^[A-Z][A-Z\\d_]*$"))"#;
+        test_capture_references(src).await;
     }
-    #[test]
-    fn it_gives_references_for_captures_2() {
+    #[tokio::test]
+    async fn it_gives_references_for_captures_2() {
         let src =
             r"(type_definition declarator: (type_identifier) @name) <REF>@defin<CURSOR>ition.type";
-        test_capture_references(src);
+        test_capture_references(src).await;
     }
-    #[test]
-    fn it_gives_references_for_captures_3() {
+    #[tokio::test]
+    async fn it_gives_references_for_captures_3() {
         let src = r"(call_expression
-    function: (identifier) <REF>@<CURSOR>function)";
-        test_capture_references(src);
+        function: (identifier) <REF>@<CURSOR>function)";
+        test_capture_references(src).await;
     }
 
     /*
      * Other
      */
-    #[test]
-    fn it_gives_references_for_captures_4() {
+    #[tokio::test]
+    async fn it_gives_references_for_captures_4() {
         let src = r#"; html(`...`), html`...`, sql(`...`), etc.
-(call_expression
-  function: (identifier) @injection.language
-  arguments: [
-    (arguments
-      (template_string) <REF>@injection.content)
-    (template_string) <REF>@injection<CURSOR>.content
-  ]
-  (#lua-match? @injection.language "^[a-zA-Z][a-zA-Z0-9]*$")
-  (#offset! <REF>@injection.content 0 1 0 -1)
-  (#set! injection.include-children)
-  ; Languages excluded from auto-injection due to special rules
-  ; - svg uses the html parser
-  ; - css uses the styled parser
-  (#not-any-of? @injection.language "svg" "css"))"#;
-        test_capture_references(src);
+    (call_expression
+      function: (identifier) @injection.language
+      arguments: [
+        (arguments
+          (template_string) <REF>@injection.content)
+        (template_string) <REF>@injection<CURSOR>.content
+      ]
+      (#lua-match? @injection.language "^[a-zA-Z][a-zA-Z0-9]*$")
+      (#offset! <REF>@injection.content 0 1 0 -1)
+      (#set! injection.include-children)
+      ; Languages excluded from auto-injection due to special rules
+      ; - svg uses the html parser
+      ; - css uses the styled parser
+      (#not-any-of? @injection.language "svg" "css"))"#;
+        test_capture_references(src).await;
     }
     // TODO: Pull out more tests from other tree-sitter repos
 }
