291c4826ad526c0106568c2077dc21c09d8ec15f
Riley Bruins <ribru17@hotmail.com>
2024-12-02 13:04:09 -0800

    test: document highlight tests (#16)

--- src/handlers/document_highlight.rs
+++ src/handlers/document_highlight.rs
@@ -93,3 +93,154 @@ pub async fn document_highlight(
         Ok(None)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use pretty_assertions::assert_eq;
+    use rstest::rstest;
+    use tower::{Service, ServiceExt};
+    use tower_lsp::lsp_types::{
+        request::DocumentHighlightRequest, DocumentHighlight, DocumentHighlightKind,
+        DocumentHighlightParams, PartialResultParams, Position, Range, TextDocumentIdentifier,
+        TextDocumentPositionParams, WorkDoneProgressParams,
+    };
+
+    use crate::test_helpers::helpers::{
+        initialize_server, lsp_request_to_jsonrpc_request, lsp_response_to_jsonrpc_response,
+        COMPLEX_FILE, TEST_URI,
+    };
+
+    type Highlight = (DocumentHighlightKind, (u32, u32), (u32, u32));
+
+    #[rstest]
+    #[case(
+        "(identifier) @variable",
+        Position { line: 0, character: 0 },
+        &[]
+    )]
+    #[case(
+        "(identifier) @variable",
+        Position { line: 0, character: 17 },
+        &[(DocumentHighlightKind::WRITE, (0, 13), (0, 22))]
+    )]
+    #[case(
+        r#"((identifier) @constant
+(#match? @constant "^[A-Z][A-Z\\d_]*$"))
+
+(boolean) @constant"#,
+        Position { line: 0, character: 17 },
+        &[
+            (DocumentHighlightKind::WRITE, (0, 14), (0, 23)),
+            (DocumentHighlightKind::READ, (1, 9), (1, 18)),
+        ]
+    )]
+    #[case(
+        r"(variable) @variable
+
+(variable
+  (type_specifier)) @variable.typed",
+        Position { line: 1, character: 6 },
+        &[
+            (DocumentHighlightKind::TEXT, (0, 1), (0, 9)),
+            (DocumentHighlightKind::TEXT, (2, 1), (2, 9)),
+        ]
+    )]
+    #[case(
+        r"expression: (number) @number
+
+expression: (boolean) @boolean",
+        Position { line: 2, character: 2 },
+        &[
+            (DocumentHighlightKind::TEXT, (0, 0), (0, 10)),
+            (DocumentHighlightKind::TEXT, (2, 0), (2, 10)),
+        ]
+    )]
+    #[case(
+        r"expression: (number) @number
+
+expression: (boolean) @boolean",
+        Position { line: 0, character: 10 },
+        &[]
+    )]
+    #[case(
+        &COMPLEX_FILE,
+        Position { line: 1, character: 5 },
+        &[
+            (DocumentHighlightKind::TEXT, (1, 4), (1, 7)),
+            (DocumentHighlightKind::TEXT, (13, 4), (13, 7)),
+            (DocumentHighlightKind::TEXT, (30, 4), (30, 7)),
+            (DocumentHighlightKind::TEXT, (31, 4), (31, 7)),
+        ]
+    )]
+    #[case(
+        &COMPLEX_FILE,
+        Position { line: 13, character: 5 },
+        &[
+            (DocumentHighlightKind::TEXT, (1, 4), (1, 7)),
+            (DocumentHighlightKind::TEXT, (13, 4), (13, 7)),
+            (DocumentHighlightKind::TEXT, (30, 4), (30, 7)),
+            (DocumentHighlightKind::TEXT, (31, 4), (31, 7)),
+        ]
+    )]
+    #[tokio::test(flavor = "current_thread")]
+    async fn document_highlight(
+        #[case] input: &str,
+        #[case] position: Position,
+        #[case] highlights: &[Highlight],
+    ) {
+        // Arrange
+        let mut service = initialize_server(&[(TEST_URI.clone(), input, Vec::new(), Vec::new())])
+            .await
+            .0;
+
+        // Act
+        let refs = service
+            .ready()
+            .await
+            .unwrap()
+            .call(lsp_request_to_jsonrpc_request::<DocumentHighlightRequest>(
+                DocumentHighlightParams {
+                    partial_result_params: PartialResultParams {
+                        partial_result_token: None,
+                    },
+                    work_done_progress_params: WorkDoneProgressParams::default(),
+                    text_document_position_params: TextDocumentPositionParams {
+                        text_document: TextDocumentIdentifier {
+                            uri: TEST_URI.clone(),
+                        },
+                        position,
+                    },
+                },
+            ))
+            .await
+            .unwrap();
+
+        // Assert
+        let actual = if highlights.is_empty() {
+            None
+        } else {
+            Some(
+                highlights
+                    .iter()
+                    .map(|(kind, p0, p1)| DocumentHighlight {
+                        kind: Some(*kind),
+                        range: Range {
+                            start: Position {
+                                line: p0.0,
+                                character: p0.1,
+                            },
+                            end: Position {
+                                line: p1.0,
+                                character: p1.1,
+                            },
+                        },
+                    })
+                    .collect(),
+            )
+        };
+        assert_eq!(
+            refs,
+            Some(lsp_response_to_jsonrpc_response::<DocumentHighlightRequest>(actual))
+        );
+    }
+}
