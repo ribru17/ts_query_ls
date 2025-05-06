5aba95abb612bd774e2ae75205b49fdfd56ea6c7
Riley Bruins <ribru17@hotmail.com>
2025-05-05 18:14:52 -0700

    test: coverage for `format --check`

--- tests/format.rs
+++ tests/format.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod test {
     use rstest::{Context, rstest};
-    use std::{env::temp_dir, fs, process::Command};
+    use std::{env::temp_dir, fs, path::Path, process::Command};
 
     #[rstest]
     #[case(
@@ -24,7 +24,7 @@ mod test {
         include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/queries/formatting_test_files/before_complex.scm")),
         include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/queries/formatting_test_files/after_complex.scm")),
     )]
-    fn cli_formatting(#[context] ctx: Context, #[case] before: &str, #[case] after: &str) {
+    fn cli_format_write(#[context] ctx: Context, #[case] before: &str, #[case] after: &str) {
         // Arrange
         let path = temp_dir()
             .join("ts-query-ls")
@@ -44,5 +44,54 @@ mod test {
         let formatted = fs::read_to_string(&path).expect("Failed to read test file");
         _ = fs::remove_file(path); // ignore cleanup errors
         assert_eq!(after, formatted);
+    }
+
+    #[rstest]
+    #[case(
+        concat!(env!("CARGO_MANIFEST_DIR"), "/queries/formatting_test_files/before_trailing_whitespace.scm"),
+        false
+    )]
+    #[case(
+        concat!(env!("CARGO_MANIFEST_DIR"), "/queries/formatting_test_files/after_trailing_whitespace.scm"),
+        true
+    )]
+    #[case(
+        concat!(env!("CARGO_MANIFEST_DIR"), "/queries/formatting_test_files/before_predicates.scm"),
+        false
+    )]
+    #[case(
+        concat!(env!("CARGO_MANIFEST_DIR"), "/queries/formatting_test_files/before_missing.scm"),
+        false
+    )]
+    #[case(
+        concat!(env!("CARGO_MANIFEST_DIR"), "/queries/formatting_test_files/before_syntax_error.scm"),
+        true // NOTE: Syntax errors are not issued formatting warnings. Revisit this?
+    )]
+    #[case(
+        concat!(env!("CARGO_MANIFEST_DIR"), "/queries/formatting_test_files/after_complex.scm"),
+        true
+    )]
+    fn cli_format_validate(#[case] path_str: &str, #[case] valid: bool) {
+        // Arrange
+        let path = Path::new(path_str);
+
+        // Act
+        let output = Command::new(env!("CARGO_BIN_EXE_ts_query_ls"))
+            .arg("format")
+            .arg("--check")
+            .arg(path)
+            .output()
+            .expect("Failed to wait on ts-query-ls format command");
+
+        // Assert
+        if valid {
+            assert!(output.stderr.is_empty());
+        } else {
+            assert!(
+                String::from_utf8(output.stderr)
+                    .unwrap()
+                    .contains("Improper formatting detected for")
+            )
+        }
     }
 }
