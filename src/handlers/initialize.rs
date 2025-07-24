b83d4a16be12a00dcdafa5920ced81e70df226a1
Riley Bruins <ribru17@hotmail.com>
2025-07-23 17:32:31 -0700

    chore: bump to v3.9.0

--- src/handlers/initialize.rs
+++ src/handlers/initialize.rs
@@ -122,7 +122,7 @@ mod test {
                 capabilities: SERVER_CAPABILITIES.clone(),
                 server_info: Some(ServerInfo {
                     name: String::from("ts_query_ls"),
-                    version: Some(String::from("3.8.0")),
+                    version: Some(String::from("3.9.0")),
                 }),
             }
         );
